f35b6db36f12f76e3e2923016d7775d0eb9c86fd
espnal <78657757+espnal@users.noreply.github.com>
2022-03-13 19:41:41 -0400

    spelling error corrected

--- cycle/game/scripting/action.py
+++ cycle/game/scripting/action.py
@@ -1,7 +1,7 @@
 class Action:
     """A thing that is done.
     
-    The responsibility of action is to do somthing that is integral or important in the game. Thus,
+    The responsibility of action is to do something that is integral or important in the game. Thus,
     it has one method, execute(), which should be overridden by derived classes.
     """
 
